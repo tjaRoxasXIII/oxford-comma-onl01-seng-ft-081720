6537cdd7a3a6a6ff6b1d1ac2fa57bd075ec66a03
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 19:24:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length > 2 
     array.pop
-    array.last << "and #{array.last}"
+    array.last << " , and #{array.last}"
     array.join(", ")
   end
 end
