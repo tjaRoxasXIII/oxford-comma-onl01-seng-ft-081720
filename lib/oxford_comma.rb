d22457cc664b70fb7afa9132ad86fc88ef4052a5
Taylor Allen <tjaroguetech@gmail.com>
2020-08-18 19:35:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2 
+    return array.join(" and ")
+  elsif array.length > 2
+    last_item = array.last
+    array.pop
+    array << "and #{last_item}"
+    array.join(", ")
+  end
 end
